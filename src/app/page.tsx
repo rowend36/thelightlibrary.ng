cb525a099f34400cdce8ebe6e07cc9478e567a6b
rowend36 <rowendduke36@gmail.com>
2024-06-20 12:29:07 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features.jsx";
+import Testimonial from "@/components/Testimonial";
+import CallToAction from "@/components/CallToAction";
+import Footer from "@/components/Footer";
+
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: todos } = await supabase.from("todos").select();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
+      <ul>
+        {todos?.map((todo: any) => (
+          <li key={undefined}>{todo}</li>
+        ))}
+      </ul>
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CallToAction />
+      <Footer />
+    </main>
+  );
+}
